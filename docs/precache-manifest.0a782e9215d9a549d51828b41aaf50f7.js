7001ba49f8fd98b0a8c5cff0f0d7dbf30f988624
Der Meyer <simon.der.meyer@gmail.com>
2020-09-13 21:39:07 +0200

    Glen Coden auto commit at 2020-09-13T19:39:07.083Z

---
+++ docs/precache-manifest.0a782e9215d9a549d51828b41aaf50f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adf9915b05a0730ecaf1ee4a6ac136f4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6fb2813eb0902e1c5e4b",
+    "url": "./static/css/main.e26654db.chunk.css"
+  },
+  {
+    "revision": "0e07d95a461bc45445c2",
+    "url": "./static/js/2.0fcd4982.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "./static/js/2.0fcd4982.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6fb2813eb0902e1c5e4b",
+    "url": "./static/js/main.479da18a.chunk.js"
+  },
+  {
+    "revision": "4a512020de93e57a9a3f",
+    "url": "./static/js/runtime-main.66bde6b1.js"
+  }
+]);
